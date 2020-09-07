6e11a34a978b1efe29c2fa5accfecbc784d8cce7
Ashutosh Muley <muleyashutosh@gmail.com>
2020-09-07 20:22:16 +0530

    Added DeleteQuery Func

--- workbench.py
+++ workbench.py
@@ -45,22 +45,44 @@ def selectFrom(tablename, conn, attributes, whereClause, key):
     curr.execute(search)
     return curr.fetchall()
 
-def updateTables(tablename,):
-    pass
+
+#DELETE FROM tablename WHERE 
+def deleteFrom(tablename, conn, whereClause, key):
+    key = ' ' + key + ' '
+    
+    where = [k + '= "' + v + '"'for k,v in whereClause.items()]
+    where = key.join(where)
+    query = 'DELETE FROM ' + tablename + ' WHERE ' + where;
+    #print(query)
+    curr = conn.cursor()
+    curr.execute(query);
+    conn.commit()
 
 
 conn = connectDB('Sample')
 db = 'employee'
 
 #display tables
-result = showTables(conn)
-for x in result:
-    print(x)
+# result = showTables(conn)
+# for x in result:
+#     print(x)
     
 #select query 
 attibutes = ['firstname', 'middlename', 'lastname']
-where = {'firstname': 'Leslie','lastname': 'Baker' }
+where = {}
 result = selectFrom(db, conn, attibutes, where, 'AND')
+
+for x in result:
+    print(x)
+
+where = {'firstname': 'Leslie','lastname': 'Baker' }
+
+deleteFrom('employee', conn, where,'AND')
+print('_______________________________')
+result = selectFrom(db, conn, attibutes, {}, 'AND')
+
+
+
 
 for x in result:
     print(x)
